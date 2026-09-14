96a11ae5fcb810fedfe4b7d3b96336582ed22a46
Anna Jang <aj3spencer@gmail.com>
2026-09-13 22:52:29 -0700

    env file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT;
 const app = express();
 app.use(express.static(__dirname));
 
